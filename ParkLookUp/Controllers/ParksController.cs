89501e3e12e6cef9a9e073cb5d7c7250993bb18b
wilkinsonmatt <wilkinsonmatt01@gmail.com>
2022-08-20 15:28:20 -0700

    changed to parks controller

---
+++ ParkLookUp/Controllers/ParksController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ParkLookUp.Models;
+
+namespace ParkLookUp.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class ParksController : ControllerBase
+  {
+    private readonly ParkLookUpContext _db;
+
+    public ParksController(ParkLookUpContext db)
+    {
+      _db = db;
+    }
+
+    // GET api/parks
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Park>>> Get()
+    {
+      return await _db.Parks.ToListAsync();
+    }
+
+    // POST api/parks
+    [HttpPost]
+    public async Task<ActionResult<Park>> Post(Park park)
+    {
+      _db.Parks.Add(park);
+      await _db.SaveChangesAsync();
+
+      return CreatedAtAction("Post", new { id = park.ParkId }, park);
+    }
+  }
+}
