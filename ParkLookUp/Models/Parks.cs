d4667a36d35481447b7f8a856b562b451a32bfea
wilkinsonmatt <wilkinsonmatt01@gmail.com>
2022-08-20 14:51:19 -0700

    park model

---
+++ ParkLookUp/Models/Parks.cs
@@ -0,0 +1,9 @@
+namespace CretaceousPark.Models
+{
+    public class Park
+    {
+        public int ParkId { get; set; }
+        public string Name { get; set; }
+        public string State { get; set; }
+    }
+}
