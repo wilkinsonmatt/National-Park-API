2ef8b9cb64f72b96c2840251c914c692c079223c
wilkinsonmatt <wilkinsonmatt01@gmail.com>
2022-08-20 19:28:42 -0700

    tweaking of the POST request for best practices

--- ParkLookUp/Controllers/ParksController.cs
+++ ParkLookUp/Controllers/ParksController.cs
@@ -31,21 +31,21 @@ namespace ParkLookUp.Controllers
       _db.Parks.Add(park);
       await _db.SaveChangesAsync();
 
-      return CreatedAtAction("Post", new { id = park.ParkId }, park);
+      return CreatedAtAction(nameof(GetPark), new { id = park.ParkId }, park);
     }
 
     // GET: api/Parks/5
     [HttpGet("{id}")]
     public async Task<ActionResult<Park>> GetPark(int id)
     {
-        var park = await _db.Parks.FindAsync(id);
+      var park = await _db.Parks.FindAsync(id);
 
-        if (park == null)
-        {
-            return NotFound();
-        }
+      if (park == null)
+      {
+        return NotFound();
+      }
 
-        return park;
+      return park;
     }
   }
 }
