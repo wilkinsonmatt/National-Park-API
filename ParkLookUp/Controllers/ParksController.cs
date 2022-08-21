c0f1113597c631c036db7dbcc558701b109806f2
wilkinsonmatt <wilkinsonmatt01@gmail.com>
2022-08-20 19:30:27 -0700

    added put

--- ParkLookUp/Controllers/ParksController.cs
+++ ParkLookUp/Controllers/ParksController.cs
@@ -47,5 +47,40 @@ namespace ParkLookUp.Controllers
 
       return park;
     }
+
+    // PUT: api/Parks/5
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, Park park)
+    {
+      if (id != park.ParkId)
+      {
+        return BadRequest();
+      }
+
+      _db.Entry(park).State = EntityState.Modified;
+
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!ParkExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+
+      return NoContent();
+    }
+
+    private bool ParkExists(int id)
+    {
+      return _db.Parks.Any(e => e.ParkId == id);
+    }
   }
 }
