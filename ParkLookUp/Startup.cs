6312f7731a859e43a5f6cd086a9d5fc57fd85f07
wilkinsonmatt <wilkinsonmatt01@gmail.com>
2022-08-21 19:22:17 -0700

    I think swagger finally works? Added userswagger and use swaggerUI. Turns out that I had V1 instead of v1.

--- ParkLookUp/Startup.cs
+++ ParkLookUp/Startup.cs
@@ -1,16 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
+using ParkLookUp.Models;
 
 namespace ParkLookUp
 {
@@ -26,22 +20,24 @@ namespace ParkLookUp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<ParkLookUpContext>(opt =>
+                opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ParkLookUp", Version = "v1" });
-            });
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ParkLookUp v1"));
             }
 
             // app.UseHttpsRedirection();
@@ -57,54 +53,3 @@ namespace ParkLookUp
         }
     }
 }
-
-
-// using Microsoft.AspNetCore.Builder;
-// using Microsoft.AspNetCore.Hosting;
-// using Microsoft.Extensions.Configuration;
-// using Microsoft.Extensions.DependencyInjection;
-// using Microsoft.Extensions.Hosting;
-// using Microsoft.EntityFrameworkCore;
-// using ParkLookUp.Models;
-
-// namespace ParkLookUp
-// {
-//     public class Startup
-//     {
-//         public Startup(IConfiguration configuration)
-//         {
-//             Configuration = configuration;
-//         }
-
-//         public IConfiguration Configuration { get; }
-
-//         // This method gets called by the runtime. Use this method to add services to the container.
-//         public void ConfigureServices(IServiceCollection services)
-//         {
-
-//             services.AddDbContext<ParkLookUpContext>(opt =>
-//                 opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
-//             services.AddControllers();
-//         }
-
-//         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-//         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-//         {
-//             if (env.IsDevelopment())
-//             {
-//                 app.UseDeveloperExceptionPage();
-//             }
-
-//             // app.UseHttpsRedirection();
-
-//             app.UseRouting();
-
-//             app.UseAuthorization();
-
-//             app.UseEndpoints(endpoints =>
-//             {
-//                 endpoints.MapControllers();
-//             });
-//         }
-//     }
-// }
